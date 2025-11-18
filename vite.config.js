9b717d3a4292456ee6728e0c933b4ea85cebc462
JungMin225 <redegg0420@gmail.com>
2025-11-18 14:02:00 +0900

    fix: GitHub Pages용 docs 빌드 복구

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Qpid/', // ✔ 레포 이름 맞음
+  base: '/Qpid/',        // GitHub Pages 서브경로
   build: {
-    outDir: 'docs',
+    outDir: 'docs',      // 빌드 결과를 docs 폴더로
   },
+  plugins: [react()],
 })
